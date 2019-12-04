5e6bca5551ec1f135706b1333d272656596ec435
Mattias Fredsberg <mattias@fredsberg.se>
2019-12-04 20:36:12 +0100

    Day4 step2

--- src/Day4.kt
+++ src/Day4.kt
@@ -1,5 +1,6 @@
 fun main() {
     println("Day 4 step 1 : " + countCandidatesBetween(124075, 580769))
+    println("Day 4 step 2 : " + countCandidatesBetweenWithUniqueDoubles(124075, 580769))
 }
 
 fun countCandidatesBetween(lower: Int, upper: Int): Int {
@@ -13,14 +14,23 @@ fun countCandidatesBetween(lower: Int, upper: Int): Int {
     return count
 }
 
+fun countCandidatesBetweenWithUniqueDoubles(lower: Int, upper: Int): Int {
+    var count = 0
+    for (n in lower..upper) {
+        val numbs = asArray(n)
+        if (isAscending(numbs) && hasUniqueDoubles(numbs)) {
+            count++
+        }
+    }
+    return count
+}
+
 fun asArray(n: Int): IntArray {
     return intArrayOf(n / 100000, (n / 10000) % 10, (n / 1000) % 10, (n / 100) % 10, (n / 10) % 10, n % 10)
 }
 
 fun isAscending(numbs: IntArray): Boolean {
-    for (i in 0..numbs.size - 2) {
-        if (numbs[i] > numbs[i + 1]) return false
-    }
+    for (i in 0..numbs.size - 2) if (numbs[i] > numbs[i + 1]) return false
     return true;
 }
 
@@ -29,8 +39,10 @@ fun hasDoubles(numbs: IntArray): Boolean {
     return false
 }
 
-private fun asNumber(actual: IntArray): Int {
-    var n = actual[0]
-    for (i in 1 until actual.size) n = n * 10 + actual[i]
-    return n
+fun hasUniqueDoubles(numbs: IntArray): Boolean {
+    if (numbs[0] == numbs[1] && numbs[0] != numbs[2]) return true // Start
+    if (numbs[5] == numbs[4] && numbs[5] != numbs[3]) return true // End
+    for (i in 2 until numbs.size - 1) // Middles
+        if (numbs[i] != numbs[i - 2] && numbs[i] == numbs[i - 1] && numbs[i] != numbs[i + 1]) return true
+    return false
 }
