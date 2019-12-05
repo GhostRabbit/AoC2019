0924c4df91499c17e3ae6829eed439371596d6aa
Mattias Fredsberg <mattias@fredsberg.se>
2019-12-05 19:59:31 +0100

    Day5

--- src/Day5.kt
+++ src/Day5.kt
@@ -1,7 +1,22 @@
 fun main() {
     Timer.measure {
         val bootloader = Inputs.readCsv("Day5.input")
-        println("Day 5 step 1 : " + bootloader.size)
-        println("Day 5 step 2 : " )
+        println("Day 5 step 1 : " + part1(bootloader))
+        println("Day 5 step 2 : " + part2(bootloader))
     }
+}
+
+fun part1(bootloader: List<Int>): Int {
+    val computer = Computer(bootloader)
+    computer.input = 1 // ID for the ship's air conditioner unit
+    computer.execute()
+    return computer.output
+}
+
+
+fun part2(bootloader: List<Int>): Int {
+    val computer = Computer(bootloader)
+    computer.input = 5 // ID for the ship's thermal radiation controller
+    computer.execute()
+    return computer.output
 }
--- src/Intcode.kt
+++ src/Intcode.kt
@@ -1,30 +1,78 @@
+import java.lang.IllegalArgumentException
+
 class Computer(bootloader: List<Int>) {
     var memory = bootloader.toMutableList()
+    var input: Int = 0
+    var output: Int = 0
 
     fun poke(ip: Int, value: Int) {
         memory[ip] = value
     }
 
+    fun read(ip: Int, mode: Int): Int {
+        if (mode == 0) // position mode
+            return memory[memory[ip]]
+        return memory[ip] // immediate mode
+    }
+
     fun execute(): Int {
         var ip = 0
         while (true) {
-            when (memory[ip]) {
-                1 -> {
-                    val p1 = memory[ip + 1]
-                    val p2 = memory[ip + 2]
+            val opcode = memory[ip] % 100
+            val mode1 = memory[ip] % 1000 / 100
+            val mode2 = memory[ip] % 10000 / 1000
+            val mode3 = memory[ip] % 100000 / 10000
+
+            when (opcode) {
+                1 -> { // Add two numbers at
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
                     val r = memory[ip + 3]
-                    memory[r] = memory[p1] + memory[p2]
+                    memory[r] = p1 + p2
                     ip += 4
                 }
-                2 -> {
-                    val p1 = memory[ip + 1]
-                    val p2 = memory[ip + 2]
+                2 -> { // Multiply two numbers at
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
                     val r = memory[ip + 3]
-                    memory[r] = memory[p1] * memory[p2]
+                    memory[r] = p1 * p2
+                    ip += 4
+                }
+                3 -> { // Store input at
+                    val r = memory[ip + 1]
+                    memory[r] = input
+                    ip += 2
+                }
+                4 -> { // Write output from
+                    output = read(ip + 1, mode1)
+                    ip += 2
+                }
+                5 -> { // jump-if-true
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
+                    ip = if (p1 != 0) p2 else ip + 3
+                }
+                6 -> { // jump-if-false
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
+                    ip = if (p1 == 0) p2 else ip + 3
+                }
+                7 -> { // less then
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
+                    val r = memory[ip + 3]
+                    memory[r] = if (p1 < p2) 1 else 0
+                    ip += 4
+                }
+                8 -> { // equals
+                    val p1 = read(ip + 1, mode1)
+                    val p2 = read(ip + 2, mode2)
+                    val r = memory[ip + 3]
+                    memory [r] = if (p1 == p2) 1 else 0
                     ip += 4
                 }
                 99 -> return memory[0]
-                else -> return -1
+                else -> throw IllegalArgumentException("Unknown opcode: $opcode")
             }
         }
     }
