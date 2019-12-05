c09d513261550e2d41aeee08c3ccc7a2caad1ddf
Mattias Fredsberg <mattias.fredsberg@cambio.se>
2019-12-05 11:24:34 +0100

    Cleanup

--- src/Day4.kt
+++ src/Day4.kt
@@ -6,25 +6,15 @@ fun main() {
 }
 
 fun countCandidatesBetween(lower: Int, upper: Int): Int {
-    var count = 0
-    for (n in lower..upper) {
-        val numbs = asArray(n)
-        if (isAscending(numbs) && hasDoubles(numbs)) {
-            count++
-        }
-    }
-    return count
+    return testEach(lower, upper) { numbs -> isAscending(numbs) && hasDoubles(numbs) }
 }
 
 fun countCandidatesBetweenWithUniqueDoubles(lower: Int, upper: Int): Int {
-    var count = 0
-    for (n in lower..upper) {
-        val numbs = asArray(n)
-        if (isAscending(numbs) && hasUniqueDoubles(numbs)) {
-            count++
-        }
-    }
-    return count
+    return testEach(lower, upper) { numbs -> isAscending(numbs) && hasUniqueDoubles(numbs) }
+}
+
+fun testEach(lower: Int, upper: Int, condition: (numbs: IntArray) -> Boolean): Int {
+    return (lower..upper).map { asArray(it) }.filter(condition).count()
 }
 
 fun asArray(n: Int): IntArray {
@@ -32,19 +22,23 @@ fun asArray(n: Int): IntArray {
 }
 
 fun isAscending(numbs: IntArray): Boolean {
-    for (i in 0..numbs.size - 2) if (numbs[i] > numbs[i + 1]) return false
+    // Very slow :(
+//    return numbs.zip(numbs.sliceArray(1 until numbs.size)).filter {it.first > it.second} .any()
+    for (i in 0 until numbs.size - 1) if (numbs[i] > numbs[i + 1]) return false
     return true;
 }
 
 fun hasDoubles(numbs: IntArray): Boolean {
-    for (i in 1 until numbs.size) if (numbs[i] == numbs[i - 1]) return true
+    // Very slow :(
+//    return numbs.zip(numbs.sliceArray(1 until numbs.size)).filter {it.first == it.second} .any()
+    for (i in 0 until numbs.size - 1) if (numbs[i] == numbs[i + 1]) return true
     return false
 }
 
 fun hasUniqueDoubles(numbs: IntArray): Boolean {
     if (numbs[0] == numbs[1] && numbs[0] != numbs[2]) return true // Start
-    if (numbs[5] == numbs[4] && numbs[5] != numbs[3]) return true // End
     for (i in 2 until numbs.size - 1) // Middles
         if (numbs[i] != numbs[i - 2] && numbs[i] == numbs[i - 1] && numbs[i] != numbs[i + 1]) return true
+    if (numbs[5] == numbs[4] && numbs[5] != numbs[3]) return true // End
     return false
 }
